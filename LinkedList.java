736d9684e759889d8ebdecc3ab931c2adfa27bf6
s3208866 <danyule@live.com.au>
2016-08-17 14:02:37 +1000

    Added/fixed a few methods
    added remove, fixed add

--- LinkedList.java
+++ LinkedList.java
@@ -1,85 +1,131 @@
 
 
-public class LinkedList{
-	private String name;
-	private Node head;
-	private Node tail;
-	private int size;
-	
-	public LinkedList(String n){
-		name = n;
-		head = null;
-		tail = null;
-		size = 0;
-	}
-	
-	public void addNode(String label){
-		Node newNode = new Node(label);
-		
-		if(head == null){
-			head = newNode;
-			tail = newNode;
-		}
-		else{
-			tail.setNext(newNode);
-			newNode.setPrev(tail);
-			tail = newNode;
-		}
-		size++;
-	}
-	
-	public Node getHead(){
-		return head;
-	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public void printNodes(){
-		Node currNode = head;
-		while(currNode != null){
-			System.out.println(currNode.getLabel());
-			currNode = currNode.getNext();
-		}
-	}
-	
-	
-	
-public static class Node {
-	private String nlabel;
-	private Node nextNode;
-	private Node prevNode;
-	
-	public Node(String label){
-		nlabel = label;
-		nextNode = null;
-		prevNode = null;
-	}
-	
-	public String getLabel(){
-		return nlabel;
-	}
-	
-	public void setLabel(String s){
-		nlabel = s;
-	}
-	
-	public Node getNext(){
-		return nextNode;
-	}
-	
-	public Node getPrev(){
-		return prevNode;
-	}
-	
-	public void setNext(Node n){
-		nextNode = n;
-	}
-	
-	public void setPrev(Node n){
-		prevNode = n;
-	}
-}
+import java.io.*;
+import java.util.*;
 
-}
+
+/**
+ * Adjacency list implementation for the FriendshipGraph interface.
+ * 
+ * Your task is to complete the implementation of this class.  You may add methods, but ensure your modified class compiles and runs.
+ *
+ * @author Jeffrey Chan, 2016.
+ */
+public class AdjList <T extends Object> implements FriendshipGraph<T>
+{   
+	
+	private LinkedList[] verts;
+	private int vertCount;
+	
+    //Contructs empty graph.	
+    public AdjList() {
+    	vertCount = 0;
+		verts = new LinkedList[vertCount];
+    } // end of AdjList()
+	
+	// START FUNCTIONS
+    public void addVertex(T vertLabel) {
+		vertCount++;
+		LinkedList[] tempList = new LinkedList[vertCount];
+		LinkedList newVert = new LinkedList((String)vertLabel);
+		
+		if(verts.length==0){
+			tempList[0] = newVert;
+		}
+		
+		else{
+			for(int x=0; x<verts.length; x++){
+					tempList[x] = verts[x];
+			}
+			tempList[verts.length] = newVert;
+		}
+	verts = tempList;
+    } 
+    // end of addVertex()
+	
+    public void addEdge(T srcLabel, T tarLabel) {
+    	
+        for(int x=0; x<verts.length; x++){
+        	if(verts[x].getName().equals((String)srcLabel)){
+    
+        		for(int y=0; y<verts.length; y++){
+        			if(verts[y].getName().matches((String)tarLabel)){
+        				verts[x].addNode((String)tarLabel);
+        				
+        					if(((String)tarLabel).matches((String) srcLabel)){
+        						break;
+        					}
+        					else{
+        						verts[y].addNode((String)srcLabel);
+        					}
+        			}
+        		}
+        	}
+        }
+    } // end of addEdge()
+	
+
+    public ArrayList<T> neighbours(T vertLabel) {
+        ArrayList<T> neighbours = new ArrayList<T>();
+        
+        // Implement me!
+        
+        return neighbours;
+    } // end of neighbours()
+    
+    
+    public void removeVertex(T vertLabel) {
+        // Implement me!
+    } // end of removeVertex()
+	
+    
+    public void removeEdge(T srcLabel, T tarLabel) {
+    	
+        for(int x=0; x<verts.length; x++){
+        	if(verts[x].getName().equals((String)srcLabel)){
+    
+        		for(int y=0; y<verts.length; y++){
+        			if(verts[y].getName().matches((String)tarLabel)){
+        				verts[x].removeNode((String) tarLabel);
+        				
+        					if(((String)tarLabel).matches((String) srcLabel)){
+        						break;
+        					}
+        					else{
+        						verts[y].removeNode((String)srcLabel);
+        					}
+        			}
+        		}
+        	}
+        }
+    } // end of removeEdges()
+	
+    
+    public void printVertices(PrintWriter os) {
+    	if(verts.length != 0){
+    		for(int x=0; x<vertCount; x++){
+        	os.println("Vertice "+(x+1)+" "+verts[x].getName());
+    		}
+    	}
+    	else{
+    		os.println("No vertices to print.");
+    	}
+    } // end of printVertices()
+	
+    
+    public void printEdges(PrintWriter os) {
+    	for(int x=0;x<verts.length;x++){
+    		
+    		verts[x].printNodes(os);
+    	}
+    } // end of printEdges()
+    
+    
+    public int shortestPathDistance(T vertLabel1, T vertLabel2) {
+    	// Implement me!
+    	
+        // if we reach this point, source and target are disconnected
+        return disconnectedDist;    	
+    } // end of shortestPathDistance()
+    
+} // end of class AdjList
